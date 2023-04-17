ee180742167ffe6af5714b8bcc37608d31aa4f46
Drzaln <rizaaru@gmail.com>
2023-04-17 11:13:40 +0700

    feat: add style

---
+++ app/styles/colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  seablue: '#90e0ef',
+  white: '#FFFFFF',
+  paperwhite: '#f0edee',
+  black: '#121212',
+  darkGray: '#2A2A2C',
+  slateGray: '#3B3C41',
+  gray: '#A4A4AB',
+  red: '#ef233c',
+};
---
+++ app/styles/layout.ts
@@ -0,0 +1,18 @@
+export const Size = {
+  xxs: 2,
+  xs: 4,
+  s: 8,
+  md: 12,
+  lg: 16,
+  xl: 24,
+  xxl: 32,
+  xxxl: 52,
+};
+
+export const BorderRadius = {
+  s: 4,
+  md: 8,
+  lg: 12,
+  xl: 16,
+  xxl: 100,
+};
